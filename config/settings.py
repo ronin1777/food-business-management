3b93dc5049ad75b0b28bc74e63d4b81d75dad13d
Hosein Sayah <57591564+ronin1777@users.noreply.github.com>
2026-09-06 09:14:45 +0330

    change db

--- config/settings.py
+++ config/settings.py
@@ -100,9 +100,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.environ.get(
+            "DB_ENGINE",
+            "django.db.backends.sqlite3",
+        ),
+        "NAME": os.environ.get(
+            "DB_NAME",
+            BASE_DIR / "db.sqlite3",
+        ),
+        "USER": os.environ.get("DB_USER", ""),
+        "PASSWORD": os.environ.get("DB_PASSWORD", ""),
+        "HOST": os.environ.get("DB_HOST", ""),
+        "PORT": os.environ.get("DB_PORT", ""),
     }
 }
 
